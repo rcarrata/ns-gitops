b694139cada9a8f43761b6a18275a5d796050d81
Luis Arizmendi <luis.ariz@gmail.com>
2021-12-14 10:07:59 +0100

    Add check script

---
+++ run-checks.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+
+echo "BOUVIER CONNECTIVITY"
+echo "## PATTY"
+oc -n bouvier exec -ti deploy/patty-deployment -- ./container-helper check
+
+echo "## SELMA"
+oc -n bouvier exec -ti deploy/selma-deployment -- ./container-helper check
+
+echo "SIMPSONS CONNECTIVITY"
+echo "## HOMER"
+oc -n simpson exec -ti deploy/homer-deployment -- ./container-helper check
+
+echo "## MARGE"
+oc -n simpson exec -ti deploy/marge-deployment -- ./container-helper check
